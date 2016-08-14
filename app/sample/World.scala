255d2985ebc2cc0abc1fd0c4da31698de5cb9881
joaoB <silva.joaobruno@gmail.com>
2016-08-14 15:47:50 -0600

    started to implement method to generate symmetric friends

--- app/sample/World.scala
+++ app/sample/World.scala
@@ -11,9 +11,11 @@ import scala.util._
 import org.springframework.scala.context.function._
 import CountingActor._
 import akka.actor.Props
+import scala.collection.mutable.ListBuffer
 
 object World {
-  val dimention = 5
+  val dimention = 50
+  val minfriendsAmount = 10
   val system = ActorSystem("FastRank")
   val emptyList = Nil
   val actors = for (i <- 0 until dimention) yield system.actorOf(Props(new CountingActor(emptyList)), name = "node" + i.toString)
@@ -21,7 +23,25 @@ object World {
   def await = {
     system.shutdown
     system.awaitTermination
-    
+  }
+
+  def generateFriends = {
+    var connections = new ListBuffer[ListBuffer[Int]]()
+    def addFriend(index: Int): Int = {
+      var r = new Random()
+      val f = r.nextInt(dimention)
+      connections(index).+=:(connections(index) match {
+        case c if !c.contains(f) => f
+        case _                   => addFriend(index)
+      })
+      f
+    }
+    val friends = for (i <- 0 until dimention) yield {
+      while (connections(i).length < minfriendsAmount) {
+        addFriend(i)
+      }
+    }
+
   }
 
 }
